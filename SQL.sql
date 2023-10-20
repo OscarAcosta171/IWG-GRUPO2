e701864575759e40597824d19d929bc2bacb9ef1
SpaceMonkeyUSM <142949483+SpaceMonkeyUSM@users.noreply.github.com>
2023-10-20 18:35:58 -0300

    Add files via upload

---
+++ SQL.sql
@@ -0,0 +1,22 @@
+--CREATE TABLE puntos(coorX FLOAT NOT NULL, coorY FLOAT NOT NULL, tipo VARCHAR(20) NOT NULL);
+/* INSERT INTO puntos(coorX, coorY, tipo)
+VALUES (1.5, 7.3, "basura"); */
+--SELECT * FROM puntos_sj;
+ /* INSERT INTO puntos 
+VALUES (3.2, 5.3, "Papel"),
+(2.4, 5.2 , "Plástico"); */
+/* ALTER TABLE puntos
+RENAME TO puntos_sj; */
+/* CREATE TABLE puntos_CC(coorX FLOAT NOT NULL, coorY FLOAT NOT NULL, tipo VARCHAR(20) NOT NULL)
+ */
+/* INSERT INTO puntos_CC
+VALUES (1,2,"Aluminio"),
+(2,3,"Basura"),
+(3,4,"Desechos nucleares"),
+(4,5,"Papel"); */
+/* SELECT * FROM puntos_CC; */
+/* CREATE TABLE campus(nombre VARCHAR(50) NOT NULL, predeterminado INTEGER NOT NULL, tabla INTEGER NOT NULL); */
+/* INSERT INTO campus
+VALUES ("San Joaquín" , 1 , 1),
+("Casa central" , 0 , 2) */
+SELECT * FROM puntos_CC;
