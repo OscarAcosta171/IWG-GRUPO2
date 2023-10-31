810b77c5f1c3e7d18af77f8782e28df25948fd38
Oscar Acosta <130588064+OscarAcosta171@users.noreply.github.com>
2023-10-31 15:59:12 -0300

    Delete myapp directory

--- myapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Marker(models.Model):
-    map_name = models.CharField(max_length=255)
-    x = models.FloatField()
-    y = models.FloatField()
-    description = models.TextField()
