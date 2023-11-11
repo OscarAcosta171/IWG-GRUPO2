70d07139619e30213d6a840c28ef7ff52b6352ef
Cris <cgonzalezb@usm.cl>
2023-11-11 00:06:37 -0300

    función agregada y cambios relacionados

--- myapp/models.py
+++ myapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Marker(models.Model):
-    map_id = models.IntegerField(max_length=255)
+    map_id = models.IntegerField()
     x = models.FloatField()
     y = models.FloatField()
     tipo = models.TextField()
--- myapp/views.py
+++ myapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import MarkerForm
 from .models import Marker
+import sqlite3
 
 # Create your views here.
 def index(request):                         #Pagina principal
@@ -35,20 +36,3 @@ def agregar_marcador(request):
     else:
         form = MarkerForm()
     return render(request, 'template.html', {'form': form})
-
-def guardar_marcadores(request):
-    if request.method == 'POST':
-        # Obtén los datos de los marcadores de la solicitud
-        marcadores = request.POST.getlist('marcadores[]')  # Ajusta el nombre según lo que estés enviando
-        # Limpia la base de datos para eliminar los marcadores antiguos
-        Marker.objects.all().delete()
-        # Guarda los nuevos marcadores en la base de datos
-        for marcador in marcadores:
-            datos = marcador.split(',')
-            if len(datos) == 4:
-                map_name, x, y, descripcion = datos
-                Marker.objects.create(map_name=map_name, x=x, y=y, description=descripcion)
-        # Redirige a la página de éxito o a donde desees
-        return redirect('nombre_de_la_vista')
-    else:
-        return render(request, 'myapp/mapa4.html')
