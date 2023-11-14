a877bf6559eeca11561762c63521bb044fb1ff23
Cris <cgonzalezb@usm.cl>
2023-11-14 17:48:52 -0300

    funcion para guardar los puntos a la base

--- myapp/models.py
+++ myapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Marker(models.Model):
-    map_name = models.CharField(max_length=255)
     x = models.FloatField()
     y = models.FloatField()
-    description = models.TextField()
+    Tipo = models.TextField()
+    mapa = models.CharField(max_length=255)
