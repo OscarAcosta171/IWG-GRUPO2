6df761d5db0c4c2ba7c57db6c08fb95f8530273e
Oscar Acosta <130588064+OscarAcosta171@users.noreply.github.com>
2023-11-21 08:45:12 -0300

    Delete myapp directory

--- myapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Marker
-
-
-# Register your models here.
-admin.site.register(Marker)
--- myapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Marker(models.Model):
-    x = models.FloatField()
-    y = models.FloatField()
-    Tipo = models.TextField()
-    color = models.TextField()
-    mapa = models.IntegerField()
