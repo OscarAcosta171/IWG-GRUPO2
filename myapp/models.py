d65c44ee4f1f8673323944e6f9456b269de75f73
Cris <cgonzalezb@usm.cl>
2023-12-04 11:11:33 -0300

    liflet v2

--- myapp/models.py
+++ myapp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Marker(models.Model):
+    id = models.AutoField(primary_key=True)
     x = models.FloatField()
     y = models.FloatField()
     Tipo = models.TextField()
